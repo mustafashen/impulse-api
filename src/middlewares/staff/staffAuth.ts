f92bf5a5eedd5ff7e309fbca6c031157bbc500e9
mustafasen <mustafasen@tuta.io>
2023-09-20 15:48:11 +0300

    added staff auth

---
+++ src/middlewares/staff/staffAuth.ts
@@ -0,0 +1,41 @@
+import { knex } from "../../db/knex";
+import {Request, Response, NextFunction} from "express"
+import { errorMessages } from "../../utils/responseMessages/errorsMessages"
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const {httpCode, message} = errorMessages("4000")
+
+async function authenticateStaff(req: Request, res: Response, next: NextFunction) {
+  try {
+    const {staff} = req.body
+
+    const token = req.headers.authorization?.replace('Bearer', '')
+    if (!token) res.status(httpCode).send({Error: message})
+
+    const decodedJWT = jwt.verify(token, process.env.JWT_SECRET)
+    if (!decodedJWT) res.status(httpCode).send({Error: message})
+
+    const foundStaff = await knex('staff').select('*').where({'id': decodedJWT.id})
+    if (foundStaff.length === 0) res.status(httpCode).send({Error: message})
+    
+    if (!foundStaff[0].isStaff) res.status(httpCode).send({Error: message})
+
+    const foundStaffToken = foundStaff[0].tokens
+    if (!foundStaffToken.includes(token)) res.status(httpCode).send({Error: message})
+
+    const foundStaffPassword = foundStaff[0].password
+    
+    const isMatch = await bcrypt.compare(staff.password, foundStaffPassword)
+    if (!isMatch) res.status(httpCode).send({Error: message})
+
+    if (isMatch) next()
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({Error: 'Error during authentication'})
+  }
+}
+
+export {
+  authenticateStaff
+}
