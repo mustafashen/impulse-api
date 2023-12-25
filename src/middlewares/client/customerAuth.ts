449f6a12e3868ef2fe35649893cfc4fbc7833db6
mustafa <mustafasen@tuta.io>
2023-12-25 20:05:53 +0300

    fixed customer auth

--- src/middlewares/client/customerAuth.ts
+++ src/middlewares/client/customerAuth.ts
@@ -3,28 +3,26 @@ import {Request, Response, NextFunction} from "express"
 import { errorMessages } from "../../utils/responseMessages/errorsMessages"
 const jwt = require('jsonwebtoken')
 
-const {httpCode, message} = errorMessages("4003")
-
 async function authenticateCustomer(req: Request, res: Response, next: NextFunction) {
   try {
 
     const token = req.headers.authorization?.replace('Bearer ', '')
     // If there is no auth token respond unauthorized
-    if (!token) throw {code: httpCode, message}
+    if (!token) throw "4003"
 
     // Verify token, if id is absent in it respond unauthorized
     const decodedJWT = jwt.verify(token, process.env.JWT_SECRET)
-    if (!decodedJWT.id) throw {code: httpCode, message}
+    if (!decodedJWT.id) "4003"
 
     // Find the customer with the id in received via token
     const foundCustomer = await knex('customer').select('id').where('id', decodedJWT.id)
-    if (foundCustomer.length === 0) throw {code: "4004", message}
+    if (foundCustomer.length === 0) throw "4003"
 
     const foundTokens = await knex('customer_token').select('token').where('customer_id', decodedJWT.id)
     const foundTokensArr = foundTokens.map((tokenObj: {token: string}) => tokenObj.token)
 
     if (!foundTokensArr.includes(token)) {
-      throw {code: httpCode, message}
+      throw "4003"
     } else {
       req.body.id = foundCustomer[0].id
       req.body.token = token
@@ -32,8 +30,8 @@ async function authenticateCustomer(req: Request, res: Response, next: NextFunct
     }
 
   } catch (error: any) {
-    console.log(error)
-    res.status(error.code).send({Error: message})
+    const {httpCode, message} = errorMessages(error)
+    res.status(httpCode).send({Error: message})
   }
 }
 
